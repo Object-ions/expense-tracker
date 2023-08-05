fcfc5e497e8736091e3c66b2c73d6b3420ffb88a
object-ions <moshikoatia@gmail.com>
2023-08-04 18:27:20 -0700

    get DOM elements

---
+++ script.js
@@ -0,0 +1,18 @@
+//Get DOM elemts
+const balance = document.getElementById('balance');
+const moneyPlus = document.getElementById('money-plus');
+const moneyMinus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('balance');
+const amount = document.getElementById('amount');
+
+const dummyTransactions = [
+  { id: 1, text: 'flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+];
+
+let transactions = dummyTransactions;
+
