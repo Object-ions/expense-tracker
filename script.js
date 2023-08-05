fc6723c1a749b0af05a3d3972e8eff87dc0f9d3c
object-ions <moshikoatia@gmail.com>
2023-08-04 19:52:03 -0700

    transactions will reflect on the DOM elements including the arthimetics of income and expense

--- script.js
+++ script.js
@@ -35,11 +35,36 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
+//Update the balance income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+                      .filter(item => item > 0)
+                      .reduce((acc, item) => (acc += item), 0)
+                      .toFixed(2);
+  // console.log(income);
+
+  const expense = amounts
+                      .filter(item => item < 0)
+                      .reduce((acc, item) => (acc += item),0)
+                      * (-1)
+                      .toFixed(2);
+  // console.log(expense);
+
+  balance.innerText = `${total}`;
+  moneyMinus.innerText = `${expense}`;
+  moneyPlus.innerText = `${income}`;
+}
+
 //Init app
 function init() {
   list.innerHTML = '';
 
   transactions.forEach(addTransactionDOM)
+  updateValues();
 }
 
 init();
